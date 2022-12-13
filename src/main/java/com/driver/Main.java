9c2a34c851ff29515e4c807889c03cf5b97ebd68
pavanvan <pavangullapalli09@gmail.com>
2022-12-13 08:06:14 +0530

    Code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,18 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Product p = new Product();
 
-        int result1 = p.product(2, 3);
-        System.out.println("Product of 2 and 3 is: " + result1);
+        int ans = p.product(10, 20);
 
-        int result2 = p.product(2, 3, 4);
-        System.out.println("Product of 2, 3, and 4 is: " + result2);
 
-        double result3 = p.product(2.0, 3.0);
-        System.out.println("Product of 2.0 and 3.0 is: " + result3);
+        int ans1 = p.product(10, 20, 30);
+
+
+        double ans2 = p.product(10.5, 9.7);
+        System.out.println(ans+" "+ans1+" "+ans2);
     }
 }
 
-class Product {
+public static class Product {
     public int product(int x, int y) {
         return x * y;
     }
